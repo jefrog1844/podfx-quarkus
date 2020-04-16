7e38343d3c5029fecb87e02c3b9a7e11d44b86b2
Jeff Rogers <jefrog1844@gmail.com>
2020-04-16 15:35:14 -0500

    make expire time configurable

--- src/main/java/com/jcr/podfx/jwt/GenerateToken.java
+++ src/main/java/com/jcr/podfx/jwt/GenerateToken.java
@@ -2,17 +2,18 @@ package com.jcr.podfx.jwt;
 
 import com.jcr.podfx.business.users.entity.User;
 import java.util.HashMap;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import org.eclipse.microprofile.jwt.Claims;
 
 public class GenerateToken {
-
+    private static final int EXPIRES_SECONDS = ConfigProvider.getConfig().getValue("com.podfx.jwt.expires", Integer.class);
+    
     public static String token(User user) throws Exception {
 
         HashMap<String, Long> timeClaims = new HashMap<>();
 
-        long duration = 300;
-        long exp = TokenUtils.currentTimeInSecs() + duration;
+        long exp = TokenUtils.currentTimeInSecs() + EXPIRES_SECONDS;
         timeClaims.put(Claims.exp.name(), exp);
 
         String token = TokenUtils.generateTokenString(user, timeClaims);
